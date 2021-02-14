18b93ad14b3bb321a6ca8d778829a02c01b8f69f
xiaog <gqp37861512>
2021-02-14 15:57:57 +0800

    init project

---
+++ src/main/java/com/yx/web/YxXgkWebjApplication.java
@@ -0,0 +1,13 @@
+package com.yx.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxXgkWebjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YxXgkWebjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yx/web/YxXgkWebjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yx.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YxXgkWebjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
